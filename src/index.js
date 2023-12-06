eefc203b8d9fe30d69a39b08d4a9d506bb04278e
EricDumrauf <eric@australbit.com>
2023-12-05 22:52:41 -0300

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//1) Importamos las librerias React y ReacDom
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+//La libreria que define que es un componente y como los componentes funcionan juntos
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from "react-dom/client";
+
+//La libreria que sabe como hacer que un componente aparezca en la pantalla del usuario
+import App from "./App";
+//2) Obtener una referencia al div con id root
+const el = document.getElementById("root");
+
+//3) Le decimos a react que tome control del elemento
+const root = ReactDOM.createRoot(el);
+
+
+
+//4)Creamos un componente
+/*
+function App(){
+    //let mensaje = "Hola buenas noches a todos/as";
+    let nombre = "Pepe";
+    let apellido = "Lopez";
+    let edad = 30;
+
+    //UTILIZAMOS {} para imrpimir variables
+    return (
+        <div>
+            <h1>Bienvenido/a usuario: {nombre} {apellido}</h1>
+            <p className="parrafos_rojos">Edad: {edad}</p>
+            <label>Ingrese su dni</label>
+            <input type="number" min={5} max={10} style={{width:"20px"}}/>
+            <textarea autoFocus/>
+        </div>
+    )
+}*/
+
+//5) Mostrar el componente en la pantalla del usuario
+root.render(<App/>);
